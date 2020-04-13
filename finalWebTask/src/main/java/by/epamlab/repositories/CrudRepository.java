44634bde798af76455a9736a582bcac38e777f95
Siarhei Pirahou <sergserg353@gmail.com>
2020-04-13 23:05:31 +0300

    final v2

--- finalWebTask/src/main/java/by/epamlab/repositories/CrudRepository.java
+++ finalWebTask/src/main/java/by/epamlab/repositories/CrudRepository.java
@@ -7,7 +7,7 @@ import java.util.List;
 public interface CrudRepository<T, ID> {
     T add(T t);
     void delete(T t);
-    T update (T t);
+    T update(T t);
     T findOne(ID id);
     List<T> findAll();
 
